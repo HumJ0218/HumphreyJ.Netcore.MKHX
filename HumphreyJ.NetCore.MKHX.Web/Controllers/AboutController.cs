dc795216c870ed56097bdeb5e2e933faf7aaaf57
姜涵 <tlos142857@live.cn>
2018-08-28 23:41:14 +0800

    增加“关于”页

---
+++ HumphreyJ.NetCore.MKHX.Web/Controllers/AboutController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HumphreyJ.NetCore.MKHX.Web.Controllers
+{
+    public class AboutController : Controller
+    {
+        [Route("about")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
