69861b3cb4185664ed83c83f0fc52f43e740b36e
姜涵 <tlos142857@live.cn>
2018-09-06 12:28:47 +0800

    优化枚举读取

--- HumphreyJ.NetCore.MKHX.Web/Models/EnumAccesser.cs
+++ HumphreyJ.NetCore.MKHX.Web/Models/EnumAccesser.cs
@@ -7,24 +7,28 @@ namespace HumphreyJ.NetCore.MKHX.Web.Models
 {
     public class EnumAccesser
     {
-        static readonly IEnumerable<Enum> Enum = new MkhxCoreContext().Enum;
 
-        public static Dictionary<int, Enum> CardColor = Enum.Where(m => m.Type == "CardColor").ToDictionary(m => m.Key, m => m);
-        public static Dictionary<int, Enum> CardColorCss = Enum.Where(m => m.Type == "CardColorCss").ToDictionary(m => m.Key, m => m);
-        public static Dictionary<int, Enum> CardRace = Enum.Where(m => m.Type == "CardRace").ToDictionary(m => m.Key, m => m);
-        public static Dictionary<int, Enum> RuneSkillConditionCompare = Enum.Where(m => m.Type == "RuneSkillConditionCompare").ToDictionary(m => m.Key, m => m);
-        public static Dictionary<int, Enum> RuneSkillConditionSlide = Enum.Where(m => m.Type == "RuneSkillConditionSlide").ToDictionary(m => m.Key, m => m);
-        public static Dictionary<int, Enum> RuneSkillConditionType = Enum.Where(m => m.Type == "RuneSkillConditionType").ToDictionary(m => m.Key, m => m);
-        public static Dictionary<int, Enum> SkillAffectType = Enum.Where(m => m.Type == "SkillAffectType").ToDictionary(m => m.Key, m => m);
-        public static Dictionary<int, Enum> SkillLaunchCondition = Enum.Where(m => m.Type == "SkillLaunchCondition").ToDictionary(m => m.Key, m => m);
-        public static Dictionary<int, Enum> SkillLaunchType = Enum.Where(m => m.Type == "SkillLaunchType").ToDictionary(m => m.Key, m => m);
-        public static Dictionary<int, Enum> MapStageDetialType = Enum.Where(m => m.Type == "MapStageDetialType").ToDictionary(m => m.Key, m => m);
-        public static Dictionary<int, Enum> RuneColor = Enum.Where(m => m.Type == "RuneColor").ToDictionary(m => m.Key, m => m);
-        public static Dictionary<int, Enum> RuneProperty = Enum.Where(m => m.Type == "RuneProperty").ToDictionary(m => m.Key, m => m);
-        public static Dictionary<int, Enum> SkillConditionCompare = Enum.Where(m => m.Type == "SkillConditionCompare").ToDictionary(m => m.Key, m => m);
-        public static Dictionary<int, Enum> RuneFragmentColor = Enum.Where(m => m.Type == "RuneFragmentColor").ToDictionary(m => m.Key, m => m);
-          public static Dictionary<int, Enum> SkillCategory = Enum.Where(m => m.Type == "SkillCategory").ToDictionary(m => m.Key, m => m);
-           public static Dictionary<int, Enum> SkillAffectTypeSide = Enum.Where(m => m.Type == "SkillAffectTypeSide").ToDictionary(m => m.Key, m => m);
-          public static Dictionary<int, Enum> SkillAffectTypePosition = Enum.Where(m => m.Type == "SkillAffectTypePosition").ToDictionary(m => m.Key, m => m);
- }
+        static readonly Dictionary<string, Dictionary<int, Enum>> Enum = new MkhxCoreContext().Enum.GroupBy(m => m.Type).ToDictionary(m => m.Key, m => m.ToDictionary(n => n.Key, n => n));
+
+        public static Dictionary<int, Enum> CardColor = Enum[nameof(CardColor)];
+        public static Dictionary<int, Enum> CardColorCss = Enum[nameof(CardColorCss)];
+        public static Dictionary<int, Enum> CardRace = Enum[nameof(CardRace)];
+
+        public static Dictionary<int, Enum> RuneColor = Enum[nameof(RuneColor)];
+        public static Dictionary<int, Enum> RuneFragmentColor = Enum[nameof(RuneFragmentColor)];
+        public static Dictionary<int, Enum> RuneProperty = Enum[nameof(RuneProperty)];
+        public static Dictionary<int, Enum> RuneSkillConditionCompare = Enum[nameof(RuneSkillConditionCompare)];
+        public static Dictionary<int, Enum> RuneSkillConditionSlide = Enum[nameof(RuneSkillConditionSlide)];
+        public static Dictionary<int, Enum> RuneSkillConditionType = Enum[nameof(RuneSkillConditionType)];
+
+        public static Dictionary<int, Enum> SkillAffectType = Enum[nameof(SkillAffectType)];
+        public static Dictionary<int, Enum> SkillAffectTypePosition = Enum[nameof(SkillAffectTypePosition)];
+        public static Dictionary<int, Enum> SkillAffectTypeSide = Enum[nameof(SkillAffectTypeSide)];
+        public static Dictionary<int, Enum> SkillCategory = Enum[nameof(SkillCategory)];
+        public static Dictionary<int, Enum> SkillLaunchCondition = Enum[nameof(SkillLaunchCondition)];
+        public static Dictionary<int, Enum> SkillLaunchType = Enum[nameof(SkillLaunchType)];
+
+        public static Dictionary<int, Enum> MapStageDetialType = Enum[nameof(MapStageDetialType)];
+
+    }
 }
