c1e6808f3fe62169e8ef8bafc48b5dd7bd060387
HumJ <tlos142857@live.cn>
2018-10-04 12:00:56 +0800

    资源浏览器

---
+++ HumphreyJ.NetCore.MKHX.Web/Controllers/OssViewerController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace HumphreyJ.NetCore.MKHX.Web.Controllers
+{
+    public class OssViewerController : Controller
+    {
+        private static readonly Dictionary<string, string> RootPath = new Dictionary<string, string> {
+            { "image", "images/" },
+            { "gamedata", "data/" },
+            { "battle", "battle/" },
+        };
+
+        [Route("ossviewer/{key}")]
+        public IActionResult Index(string key)
+        {
+            try
+            {
+                return View(RootPath[key]);
+            }
+            catch
+            {
+                return new NotFoundResult();
+            }
+        }
+    }
+}
